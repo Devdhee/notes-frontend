ee7f42e86b63bb1b0f602b282974c5174544a433
Devdhee <kvngdhavid@gmail.com>
2024-07-11 10:17:31 +0100

    feature(GlobalStyles, App) I installed the styled components library and defined the global styles to be used across the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import "./App.css";
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 48px;
+  text-align: center;
+  color: var(--color-red-700);
+`;
 
 function App() {
-  return <div> Hello World</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <H1>Hello World</H1>
+    </>
+  );
 }
 
 export default App;
